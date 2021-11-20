4d464271a6c64fa06742b5e69849d4339905db50
Thomas <hirtzt@gmail.com>
2021-11-20 09:47:05 +0100

    Update utilities.py

--- notion_scholar/utilities.py
+++ notion_scholar/utilities.py
@@ -1,10 +1,22 @@
-def merge_files(input_path: str, bibliography_path: str) -> None:
+class NotionScholarError(Exception):
+    """A base class for notion-scholar exceptions."""
+
+
+def append_string_to_file(string: str, file_path: str):
+    with open(file_path, 'a') as f:
+        f.write('\n')
+        f.write(string)
+
+
+def merge_files(input_path: str, output_file: str) -> None:
     with open(input_path, 'br') as f:
         content = f.read()
 
-    with open(bibliography_path, 'ba') as f:
+    with open(output_file, 'ba') as f:
         f.write(b'\n')
         f.write(content)
 
-    with open(input_path, 'w'):
+
+def clean_file(file_path: str) -> None:
+    with open(file_path, 'w'):
         pass
