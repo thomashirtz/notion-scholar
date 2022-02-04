1bedae1210fce89ca93659eb017b10faa04a2005
Thomas <hirtzt@gmail.com>
2022-02-04 21:40:25 +0100

    Add docstrings

--- notion_scholar/publication.py
+++ notion_scholar/publication.py
@@ -3,6 +3,7 @@ from typing import NamedTuple
 
 
 class Publication(NamedTuple):
+    """NamedTuple object used to store a bibtex publication reference."""
     key: str
     title: str
     authors: str
@@ -20,4 +21,13 @@ def filter_publication_list(
         publication_list: List[Publication],
         key_list_to_exclude: List[str],
 ) -> List[Publication]:
+    """Filter a list of "Publication" object using a list of keys to exclude.
+
+    Args:
+        publication_list: list of "Publication" objects.
+        key_list_to_exclude: list of keys to exclude.
+
+    Returns:
+        Filtered list of "Publication"s.
+    """
     return [p for p in publication_list if p.key not in key_list_to_exclude]
