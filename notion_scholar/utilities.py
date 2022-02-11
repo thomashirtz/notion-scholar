b0a49ffcfadbd6ee986a2598a371f8cd11280164
Thomas <hirtzt@gmail.com>
2022-02-11 20:48:12 +0100

    Add docstrings

--- notion_scholar/utilities.py
+++ notion_scholar/utilities.py
@@ -6,15 +6,35 @@ class NotionScholarError(Exception):
 
 
 def append_string_to_file(file_path: str, content: str) -> None:
+    """Append content to a file.
+
+    Args:
+        file_path: path of the file that will be appended to.
+        content: content that will be appended.
+    """
     with open(file_path, 'a') as f:
         f.write('\n')
         f.write(content)
 
 
 def get_duplicates(lst: list) -> list:
+    """Get duplicates from a list.
+
+    Args:
+        lst: list that will be inspected.
+
+    Returns:
+        List of duplicates from the list.
+    """
     return [k for k, v in Counter(lst).items() if v > 1]
 
 
-def write_to_file(file_path: str, content: str):
+def write_to_file(file_path: str, content: str) -> None:
+    """Write content to a file.
+
+    Args:
+        file_path: path of the file that will be written to.
+        content: content that will be written.
+    """
     with open(file_path, 'w') as f:
         f.write(content)
