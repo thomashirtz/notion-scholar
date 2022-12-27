9ef6891babf618c39a004315de8c848daff92feb
Thomas <hirtzt@gmail.com>
2022-12-27 18:17:45 +0800

    Add docstring

--- notion_scholar/download.py
+++ notion_scholar/download.py
@@ -7,6 +7,17 @@ def download(
         token: str,
         database_id: str,
 ) -> int:
+    """Write the bibliography from the database `database_id` in the file
+    located at `file_path`.
+
+    Args:
+        file_path: File path in which the bibliography will be saved.
+        token: Notion API token.
+        database_id: Targeted database id.
+
+    Returns:
+        Error code.
+    """
     bibtex_str_list = get_bibtex_string_list_from_database(
         token=token,
         database_id=database_id,
