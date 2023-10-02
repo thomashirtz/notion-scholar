1a24931fca5377f700926ef1f1d04929b1e39bf4
tricuspidone46 <129996564+tricuspidone46@users.noreply.github.com>
2023-10-02 19:25:33 -0400

    Update notion_api.py

--- notion_scholar/notion_api.py
+++ notion_scholar/notion_api.py
@@ -84,7 +84,7 @@ def add_publications_to_database(
                 'Authors': Property.rich_text(authors),
                 'Year': Property.number(publication.year),
                 'URL': Property.url(publication.url),
-                'Category': Property.select(publication.keywords),
+                'Category': Property.relation(publication.keywords),
                 'Inbox': Property.checkbox(True),
                 'Type': Property.select(publication.type),
                 'DOI': Property.rich_text(publication.doi),
