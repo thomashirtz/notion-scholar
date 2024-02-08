dcbf180816df0555f038a542f23f546fc328395d
Thomas Hirtz <hirtzt@gmail.com>
2024-02-08 10:20:21 +0100

    Update docstrings

--- notion_scholar/bibtex.py
+++ notion_scholar/bibtex.py
@@ -9,14 +9,18 @@ from notion_scholar.publication import Publication
 
 
 def get_bib_database_from_file(file_path: str) -> BibDatabase:
-    """Read a file and parse the BibTex entries present in it.
+    """
+    Parses a BibTeX file to create a BibDatabase object.
+
+    This function reads a BibTeX file specified by the file path, parses the
+    BibTeX entries within it, and returns a BibDatabase object containing all
+    the parsed entries.
 
     Args:
-        file_path: path of the file that needs to be parsed.
+        file_path (str): The path to the BibTeX file to be parsed.
 
     Returns:
-        BibDatabase object that contains the instances present in the
-        file.
+        BibDatabase: An object containing the parsed BibTeX entries from the file.
     """
     with open(file_path, encoding='utf-8') as bibtex_file:
         parser = BibTexParser(
@@ -27,14 +31,18 @@ def get_bib_database_from_file(file_path: str) -> BibDatabase:
 
 
 def get_bib_database_from_string(string: str) -> BibDatabase:
-    """Create a BibDatabase object from a string of BibTex entries.
+    """
+    Converts a string containing BibTeX entries into a BibDatabase object.
+
+    This function takes a string that contains one or more BibTeX entries,
+    parses these entries, and returns a BibDatabase object that encapsulates
+    the parsed entries.
 
     Args:
-        string: string containing the BibTex entries.
+        string (str): A string containing BibTeX entries.
 
     Returns:
-        BibDatabase object that contains the instances present in the
-        string.
+        BibDatabase: An object containing the parsed BibTeX entries.
     """
     bibtex_parser = BibTexParser(
         interpolate_strings=False,
@@ -45,7 +53,19 @@ def get_bib_database_from_string(string: str) -> BibDatabase:
 
 
 def get_bibtex_str(entry: dict) -> str:
-    """"""
+    """
+    Generates a BibTeX string from a single entry dictionary, excluding the 'abstract' field if the resulting string is too long.
+
+    This function takes a dictionary representing a single BibTeX entry, converts
+    it to a BibTeX-formatted string, and returns it. If the generated string exceeds
+    2000 characters, the 'abstract' field is omitted to reduce size.
+
+    Args:
+        entry (dict): A dictionary representing a single BibTeX entry.
+
+    Returns:
+        str: A BibTeX-formatted string. Returns an empty string if the resulting string without the 'abstract' is still over 2000 characters.
+    """
     database = BibDatabase()
     database.entries = [dict(entry)]
     bibtex_str = dumps(database)
@@ -60,13 +80,19 @@ def get_bibtex_str(entry: dict) -> str:
 
 
 def get_publication_list(bib_database: BibDatabase) -> List[Publication]:
-    """Convert a BibDatabase object into a list of "Publication"s.
+    """
+    Converts a BibDatabase object into a list of Publication objects.
+
+    This function iterates over each entry in a BibDatabase object, creates a
+    Publication object for each entry, and returns a list of these Publication
+    objects. Each Publication object contains bibliographic information such as
+    title, authors, year, etc., extracted from the BibDatabase entries.
 
     Args:
-        bib_database: a BibDatabase object.
+        bib_database (BibDatabase): The BibDatabase object containing BibTeX entries.
 
     Returns:
-        List of "Publication"s.
+        List[Publication]: A list of Publication objects created from the BibDatabase entries.
     """
     publications = []
     for entry in bib_database.entries:
@@ -88,13 +114,18 @@ def get_publication_list(bib_database: BibDatabase) -> List[Publication]:
 
 
 def get_key_list(bib_file_path: str) -> list:
-    """Read a BibTex file and return the list of ID of the paper present in it.
+    """
+    Extracts and returns the list of IDs from a BibTeX file.
+
+    This function reads a BibTeX file, parses it to extract all entries, and
+    then compiles a list of the 'ID' fields from these entries, which typically
+    serve as unique identifiers for the bibliographic entries.
 
     Args:
-        bib_file_path: path of the file that needs to be inspected.
+        bib_file_path (str): The file path to the BibTeX file.
 
     Returns:
-        List of IDs.
+        list: A list of string IDs from the BibTeX file entries.
     """
     bib_database = get_bib_database_from_file(bib_file_path)
     return [entry['ID'] for entry in bib_database.entries]
